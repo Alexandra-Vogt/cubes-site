1d420565c5942e5791a6aaac15c53070e82c680f
Alex Vogt <accounts@vogt.systems>
2020-07-06 11:36:23 -0700

    changed colorscheme

--- js/optimized/cubes/render.js
+++ js/optimized/cubes/render.js
@@ -3,6 +3,11 @@ goog.provide('cubes.render');
 goog.require('cljs.core');
 goog.require('cljs.core.constants');
 goog.require('quil.core');
+cubes.render.gvred = new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(251),(73),(52)], null);
+cubes.render.gvblue = new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(131),(165),(152)], null);
+cubes.render.gvyellow = new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(250),(189),(47)], null);
+cubes.render.gvdark = new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(29),(32),(33)], null);
+cubes.render.gvgrey = new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(40),(40),(40)], null);
 cubes.render.render_square = (function cubes$render$render_square(p__6543,x,y,thickness,size){
 var vec__6544 = p__6543;
 var r = cljs.core.nth.cljs$core$IFn$_invoke$arity$3(vec__6544,(0),null);
@@ -13,7 +18,7 @@ quil.core.fill.cljs$core$IFn$_invoke$arity$3(r,g,b);
 
 quil.core.rect.cljs$core$IFn$_invoke$arity$4(x,y,size,size);
 
-quil.core.fill.cljs$core$IFn$_invoke$arity$1((0));
+quil.core.fill.cljs$core$IFn$_invoke$arity$1(cubes.render.gvdark);
 
 return quil.core.rect.cljs$core$IFn$_invoke$arity$4((thickness + x),(thickness + y),inner,inner);
 });
@@ -22,21 +27,21 @@ var map__6548 = p__6547;
 var map__6548__$1 = (((((!((map__6548 == null))))?(((((map__6548.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__6548.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__6548):map__6548);
 var x = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__6548__$1,cljs.core.cst$kw$x);
 var y = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__6548__$1,cljs.core.cst$kw$y);
-return cubes.render.render_square(new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(255),(255),(0)], null),x,y,2.5,(20));
+return cubes.render.render_square(cubes.render.gvyellow,x,y,2.5,(20));
 });
 cubes.render.render_enemy = (function cubes$render$render_enemy(p__6550){
 var map__6551 = p__6550;
 var map__6551__$1 = (((((!((map__6551 == null))))?(((((map__6551.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__6551.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__6551):map__6551);
 var x = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__6551__$1,cljs.core.cst$kw$x);
 var y = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__6551__$1,cljs.core.cst$kw$y);
-return cubes.render.render_square(new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(255),(0),(0)], null),x,y,2.5,(20));
+return cubes.render.render_square(cubes.render.gvred,x,y,2.5,(20));
 });
 cubes.render.render_point_cube = (function cubes$render$render_point_cube(p__6553){
 var map__6554 = p__6553;
 var map__6554__$1 = (((((!((map__6554 == null))))?(((((map__6554.cljs$lang$protocol_mask$partition0$ & (64))) || ((cljs.core.PROTOCOL_SENTINEL === map__6554.cljs$core$ISeq$))))?true:false):false))?cljs.core.apply.cljs$core$IFn$_invoke$arity$2(cljs.core.hash_map,map__6554):map__6554);
 var x = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__6554__$1,cljs.core.cst$kw$x);
 var y = cljs.core.get.cljs$core$IFn$_invoke$arity$2(map__6554__$1,cljs.core.cst$kw$y);
-return cubes.render.render_square(new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [(0),(0),(255)], null),x,y,2.5,(20));
+return cubes.render.render_square(cubes.render.gvblue,x,y,2.5,(20));
 });
 cubes.render.render_entities = (function cubes$render$render_entities(state){
 quil.core.text.cljs$core$IFn$_invoke$arity$3(cljs.core.str.cljs$core$IFn$_invoke$arity$1(cljs.core.cst$kw$spawned_QMARK_.cljs$core$IFn$_invoke$arity$1(state)),(100),(100));
@@ -54,11 +59,13 @@ return cubes.render.render_player(cljs.core.cst$kw$player.cljs$core$IFn$_invoke$
 }finally {quil.core.pop_matrix();
 }});
 cubes.render.render_state = (function cubes$render$render_state(state){
-quil.core.background.cljs$core$IFn$_invoke$arity$1((0));
+quil.core.background.cljs$core$IFn$_invoke$arity$3((29),(32),(33));
+
+quil.core.stroke.cljs$core$IFn$_invoke$arity$3((29),(32),(33));
 
 cubes.render.render_entities(state);
 
-quil.core.fill.cljs$core$IFn$_invoke$arity$3((255),(255),(0));
+quil.core.fill.cljs$core$IFn$_invoke$arity$1(cubes.render.gvyellow);
 
 return quil.core.text.cljs$core$IFn$_invoke$arity$3([cljs.core.str.cljs$core$IFn$_invoke$arity$1(cljs.core.cst$kw$text.cljs$core$IFn$_invoke$arity$1(state)),((cljs.core._EQ_.cljs$core$IFn$_invoke$arity$2((cljs.core.mod(quil.core.seconds(),(2)) | (0)),(0)))?"\u2588":"")].join(''),(10),(20));
 });
